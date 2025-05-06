2349e1268058df7563ad9853ae286cc1696a66b9
Ramir <ramirladanga@gmail.com>
2025-05-06 21:16:49 +0800

    [feat]: upload extension

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: AuthState = {
-  isAuthenticated: false,
+  isAuthenticated: true,
   user: null,
   loading: false,
   error: null,
